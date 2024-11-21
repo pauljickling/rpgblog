b462a52841d41405084a0097a8a4c45fa3a21832
Paul Jickling <paul.jickling@ethereum.org>
2024-11-21 13:49:01 -0500

    use underscores for pathing
    Signed-off-by: Paul Jickling <paul.jickling@ethereum.org>

--- blog.sh
+++ blog.sh
@@ -1,23 +1,32 @@
 #!/bin/bash
 set -eu
 
+# Set blog title to argument
+
 TITLE="$1"
 
+# Get date for blog
 DATE="$(date +%Y-%m-%d)"
 
+# Must supply title as argument to script
 if [[ $# -eq 0 ]] ; then
     echo "Missing title argument"
     exit 1
 fi
 
+# Markdown header template
 MARKDOWN_HEADER="---
 layout: post
 date: $DATE
 title: \"$TITLE\"
 ---"
 
-FILE_PATH="_posts/$DATE-$TITLE.md"
+TITLE_PATH="${TITLE// /_}"
+# Assign file path
+FILE_PATH="_posts/$DATE-$TITLE_PATH.md"
 
+# Write to filepath
 echo "$MARKDOWN_HEADER" > "$FILE_PATH"
 
+# Let user know task complete and location of file
 echo "Markdown header generated at $FILE_PATH"
